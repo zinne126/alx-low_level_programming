a916a1a44f17d4865ecdb226dea598d7d25f5ebf
zinne126 <ohaeriezinne7@gmail.com>
2023-04-20 14:07:56 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a c line code that uses printf function
+ *
+ * Return:0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art/\n");
+	return (0);
