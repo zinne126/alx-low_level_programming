f8b28fdd89d21bb1914446b7fa9d5685023fb309
zinne126 <ohaeriezinne7@gmail.com>
2023-04-20 06:25:18 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printf a c line code using puts
+ *
+ * Return:0
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
