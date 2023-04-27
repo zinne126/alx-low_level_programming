495fd8bd8ca204b9abd188fc15b3e16324c2f0d3
zinne126 <ohaeriezinne7@gmail.com>
2023-04-27 12:19:56 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-isupper.c -o 0-isuper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for lowercase character
+ * @c: The character to be checked
+ * Return: 1 for uppercase character or 0 for anything else
+ */
+
+int _isupper(int c)
+
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
